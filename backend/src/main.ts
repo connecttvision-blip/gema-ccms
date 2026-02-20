9472da281d9f18c9dd1d3209b696714c1b3685f3
connecttvision-blip <connecttvision@gmail.com>
2026-02-20 18:09:54 -0300

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,43 +1,14 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Request, Response, NextFunction } from 'express';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-  origin: true,
-  credentials: true,
-  allowedHeaders: [
-    'Content-Type',
-    'Authorization',
-    'x-tenant-id',
-  ],
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-});
-
-  app.use((req: Request, res: Response, next: NextFunction) => {
-    // ✅ liberar preflight CORS
-    if (req.method === 'OPTIONS') {
-      return res.sendStatus(204);
-    }
-
-    const openPaths = ['/health'];
-
-    if (openPaths.includes(req.path)) {
-      return next();
-    }
-
-    const tenantId = req.headers['x-tenant-id'] as string;
-
-    if (!tenantId) {
-      return res.status(400).json({ error: 'x-tenant-id header required' });
-    }
-
-    (req as any).tenantId = tenantId;
-    next();
+    origin: true,
+    credentials: true,
   });
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
